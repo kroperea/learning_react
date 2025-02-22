66a2e6eb361cd0b0e02563d0d05e6df523986cd5
Carolina Perea <kroperea@Carolinas-MacBook-Pro.local>
2025-02-22 11:58:08 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
 import './App.css'
+import chef from './images/photo.jpg'
 let heart = '❤️'
 
-function Header() {
+function Header({ name, year }) {
+  // child component
   return (
     <header>
-      <h1> Caro's kitchen {heart} </h1>
+      <h1>
+        {name}&apos;s kitchen {heart}
+      </h1>
+      <p> Copyright {year}</p>
     </header>
   )
 }
 
+const items = [
+  'Macaroni and Cheese',
+  'Salmon with Potatoes',
+  'Tofu with Vegetables',
+]
+
+const dishObjects = items.map((dish, i) => ({ id: i, title: dish }))
+console.log(dishObjects)
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li key={dish.id} style={{ listStyle: 'none' }}>
+          {dish.title}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 function App() {
+  // parent component
   return (
     <div>
-      <Header />
-      <main>
-        <h2> We serve the most delicious food around</h2>
-      </main>
+      <Header name='Caro' year={new Date().getFullYear()} />
+      <Main dishes={dishObjects} />
     </div>
   )
 }

491b88e789f248c4c020fed7e3b9458b524ce84a
Carolina Perea <kroperea@Carolinas-MacBook-Pro.local>
2025-02-24 13:45:23 +0100

    thrid commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import chef from './images/photo.jpg'
+import { useState } from 'react'
 let heart = '❤️'
 
 function Header({ name, year }) {
@@ -23,24 +24,50 @@ const items = [
 const dishObjects = items.map((dish, i) => ({ id: i, title: dish }))
 console.log(dishObjects)
 
-function Main({ dishes }) {
+function Main({ dishes, openStatus, onStatus }) {
   return (
-    <ul>
-      {dishes.map((dish) => (
-        <li key={dish.id} style={{ listStyle: 'none' }}>
-          {dish.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <div>
+        <button onClick={() => onStatus(true)}> I want to be open </button>
+        <h2>
+          Welcome to this beautiful restaurant! {openStatus ? 'Open' : 'Closed'}
+        </h2>
+      </div>
+      <main>
+        <img
+          src={chef}
+          alt="image of the restaurant's chef"
+          className='chef'
+          style={{ maxWidth: '20%' }}
+        />
+        <section>
+          <p>We serve the most delicious food around.</p>
+          <h3>Check out our menu:</h3>
+          <ul>
+            {dishes.map((dish) => (
+              <li key={dish.id} style={{ listStyle: 'none' }}>
+                {dish.title}
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </>
   )
 }
 
 function App() {
   // parent component
+  const [status, setStatus] = useState(true)
+  console.log(status)
   return (
     <div>
+      <h1> The restaurant is currently {status ? 'Open' : 'Closed'}. </h1>
+      <button onClick={() => setStatus(!status)}>
+        {status ? 'close' : 'open'} restaurant
+      </button>
       <Header name='Caro' year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <Main dishes={dishObjects} openStatus={status} onStatus={setStatus} />
     </div>
   )
 }
